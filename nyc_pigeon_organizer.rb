e5f187b6bec42e89c80feb882d9ae9283e16a793
Keenan Alves <keenan.alves@gmail.com>
2019-09-11 18:00:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,17 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   
   data.each do |pigeondata, value|
-    value.each do ||
+    value.each do |stats, allnames|
+      allnames.each do |name|
+        if pigeon_list[name] == nil 
+          pigeon_list[name] = {}
+        end 
+        if pigeon_list[name][pigeondata] == nil 
+          pigeon_list[name][pigeondata] = []
+        end
+        pigeon_list[name][pigeondata] << stats.to_s 
+      end
+    end
+  end 
+  return pigeon_list 
 end
